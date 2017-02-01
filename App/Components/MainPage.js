1293eac9b2bb6c7ac09b847707881b2f986a90c5
luccafort <luccafort@gmail.com>
2017-02-01 10:57:06 +0900

    :tada: connpassAPIのReceiverを追加
    イベント情報を受け取る機能を追加
    Detail.jsに細かな修正(タイプミスやimport漏れ)を追加

--- App/Components/MainPage.js
+++ App/Components/MainPage.js
@@ -17,10 +17,22 @@ export default class MainPage extends Component {
 		this.props.navigator.push({name: "detail"});
 	}
 
+	componentWillMount() {
+		var url = "http://connpass.com/api/v1/event";
+		var req = new XMLHttpRequest();
+		req.open("GET", url);
+		req.onload = () => {
+			console.log(JSON.parse(req.responseText));
+		};
+		req.send();
+	}
+
 	render() {
 		return (
-			<View style={Style.container}>
-				<Text onPress = {this._onPress}>this is MainPage</Text>
+			<View style = {Style.container}>
+				<Text onPress = {this._onPress}>
+					this is MainPage
+				</Text>
 			</View>
 		);
 	}
